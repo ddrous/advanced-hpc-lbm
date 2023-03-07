35b79c7683d422fe49611cf5ef7ac2c2cff4962f
RDES (gb21553) <rd.nzoyemngueguin@bristol.ac.uk>
2023-03-07 11:48:11 +0000

    checked submission

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
 
-module load  icc/2017.1.132-GCC-5.4.0-2.26
+module load icc/2017.1.132-GCC-5.4.0-2.26
